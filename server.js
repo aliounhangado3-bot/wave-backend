ed812ec9190e7a20476a1b46a900a10061fdf388
Aliou NHANGADO <badarabintou@outlook.fr>
2026-08-05 00:20:41 +0000

    Enable JSON parsing

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.use(express.json());
+app.use(express.application/json());
 app.use(express.urlencoded({ extended: true }));
 
 // Route de test
